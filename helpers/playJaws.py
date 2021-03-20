9a0f3f0ef6849a92327485572569fb456da4a70d
braedenn <braeden.norman6@gmail.com>
2021-03-20 01:46:34 -0700

    edited file structure

--- helpers/playJaws.py
+++ helpers/playJaws.py
@@ -5,14 +5,14 @@ import time
 
 
 def playJaws():
-    jaws = (os.getcwd() + "/sounds/jaws.mp3")
+    jaws = os.getcwd()+ "\sounds\jaws.mp3"
     playsound.playsound(jaws)
 
 
 if __name__ == '__main__':
     process = multiprocessing.Process(target=playJaws) # ,args = (1,2) 
     process.start() 
-    time.sleep(5)
+    time.sleep(6)
     process.terminate()
     print("done")
     time.sleep(5)
--- main.py
+++ main.py
@@ -4,92 +4,10 @@ from win32api import GetSystemMetrics
 import pyautogui
 import time
 import random
-import ctypes
 import time
 import os
 import win32api
-
-
-SendInput = ctypes.windll.user32.SendInput
-
-# key codes https://wiki.nexusmods.com/index.php/DirectX_Scancodes_And_How_To_Use_Them
-W = 0x11
-A = 0x1E
-S = 0x1F
-D = 0x20
-KEY_1 = 0x02
-KEY_2 = 0x03
-KEY_3 = 0x04
-KEY_4 = 0x05
-KEY_5 = 0x06
-KEY_6 = 0x07
-KEY_7 = 0x08
-KEY_8 = 0x09
-KEY_9 = 0x10
-
-########################################################################
-# Code from https://github.com/Sentdex/pygta5/blob/master/directkeys.py
-#######################################################################
-PUL = ctypes.POINTER(ctypes.c_ulong)
-class KeyBdInput(ctypes.Structure):
-    _fields_ = [("wVk", ctypes.c_ushort),
-                ("wScan", ctypes.c_ushort),
-                ("dwFlags", ctypes.c_ulong),
-                ("time", ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
-
-class HardwareInput(ctypes.Structure):
-    _fields_ = [("uMsg", ctypes.c_ulong),
-                ("wParamL", ctypes.c_short),
-                ("wParamH", ctypes.c_ushort)]
-
-class MouseInput(ctypes.Structure):
-    _fields_ = [("dx", ctypes.c_long),
-                ("dy", ctypes.c_long),
-                ("mouseData", ctypes.c_ulong),
-                ("dwFlags", ctypes.c_ulong),
-                ("time",ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
-
-class Input_I(ctypes.Union):
-    _fields_ = [("ki", KeyBdInput),
-                 ("mi", MouseInput),
-                 ("hi", HardwareInput)]
-
-class Input(ctypes.Structure):
-    _fields_ = [("type", ctypes.c_ulong),
-                ("ii", Input_I)]
-
-# Actuals Functions
-
-def PressKey(hexKeyCode):
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-
-def ReleaseKey(hexKeyCode):
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-############################################################################################
-
-def getKeyCode(key):
-    switch = {
-        1: KEY_1,
-        2: KEY_2,
-        3: KEY_3,
-        4: KEY_4,
-        5: KEY_5,
-        6: KEY_6,
-        7: KEY_7,
-        8: KEY_8,
-        9: KEY_9,
-    }
-    return switch.get(key)
+from Helpers.gtaKeyPress import *
 
 def isGreen(pixel):
     return (pixel[1] > (pixel[0] + DIFFERENCE_GREEN) and pixel[1] > (pixel[2] + DIFFERENCE_GREEN)) #and pixel[1] > 200)
